dd4ba4dcb96a1585bf5fa3c6ac346c7aab7c8aa5
skc4365 <50658047+skc4365@users.noreply.github.com>
2024-09-12 22:29:46 +0900

    web_mvc_thymeleaf

---
+++ ex0912/MVCModel/src/main/java/com/example/demo/MvcModelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcModelApplication.class, args);
+	}
+
+}
